71e8d71bbce5a8ffd09ace08513c023eabebeb49
Rizone Hasan <reznuhasan31@gmail.com>
2025-06-04 02:42:51 +0600

    Add Navbar design

---
+++ src/page/Header/Header.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './Header.css';
+import logo from "../../assets/logo.png"
+const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobileMenuOpen(prev => !prev);
+  };
+  return (
+   <header className="header">
+      <div className="header-inner">
+        <div className="left">
+          <button className="hamburger" onClick={toggleMenu}>☰</button>
+          <div className="logo">
+           <img src={logo} />
+          </div>
+        </div>
+
+        <nav className={`nav ${isMobileMenuOpen ? 'open' : ''}`}>
+          <a href="#">FAQ</a>
+          <a href="#">Reviews</a>
+          <a href="#">Pricing</a>
+        </nav>
+
+        <button className="get-started">Get started</button>
+      </div>
+
+      {/* Mobile nav (below header) */}
+      {isMobileMenuOpen && (
+        <div className="mobile-nav">
+          <a href="#">FAQ</a>
+          <a href="#">Reviews</a>
+          <a href="#">Pricing</a>
+        </div>
+      )}
+    </header>
+  )
+  
+};
+
+export default Header;
