500e99e383d774cdbaab38d2c7fbfd92348356cd
Rizone Hasan <reznuhasan31@gmail.com>
2025-06-04 05:21:28 +0600

    alert part done

--- src/page/Email/Email.jsx
+++ src/page/Email/Email.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { browserListArr } from '../../DB/BrowserList';
 import BrowserCard from '../../component/BrowserCard/BrowserCard';
 import "./Email.css"
+import avatar from "../../assets/EmailAvatar.png"
 const Email = () => {
     const allBrowser=browserListArr;
 
@@ -11,6 +12,16 @@ const Email = () => {
        {
         allBrowser.map(browser=><BrowserCard browser={browser}/>)
        }
+      </div>
+      <div className='email-alert-part'>
+        <div className='alert-content'>
+             <h1 className='red-content'>12</h1>
+             <h1 className='red-content'>Data Breach Alert</h1>
+             <p> Your email address has appeared in a known data breach. Check the details below to find out where and how your information may have been compromised.</p>
+        </div>
+        <div >
+            <img src={avatar} alt="" />
+        </div>
       </div>
     </div>
   )
