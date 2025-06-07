8d48bcdcaa47cb5b41488a9b63fb2aa063d44296
Rizone Hasan <reznuhasan31@gmail.com>
2025-06-07 16:11:56 +0600

    data part add done

--- src/page/Email/Email.jsx
+++ src/page/Email/Email.jsx
@@ -3,28 +3,39 @@ import { browserListArr } from '../../DB/BrowserList';
 import BrowserCard from '../../component/BrowserCard/BrowserCard';
 import "./Email.css"
 import avatar from "../../assets/EmailAvatar.png"
+import avatar1 from "../../assets/GirlAvatar1.png"
 import BreachCards from '../../component/BreachCard/BreachCards';
 const Email = () => {
-    const allBrowser=browserListArr;
+  const allBrowser = browserListArr;
 
   return (
     <div className='email-container'>
       <div className='browser-all-card'>
-       {
-        allBrowser.map(browser=><BrowserCard browser={browser}/>)
-       }
+        {
+          allBrowser.map(browser => <BrowserCard browser={browser} />)
+        }
       </div>
       <div className='email-alert-part'>
         <div className='alert-content'>
-             <h1 className='red-content'>12</h1>
-             <h1 className='red-content'>Data Breach Alert</h1>
-             <p> Your email address has appeared in a known data breach. Check the details below to find out where and how your information may have been compromised.</p>
+          <h1 className='red-content'>12</h1>
+          <h1 className='red-content'>Data Breach Alert</h1>
+          <p> Your email address has appeared in a known data breach. Check the details below to find out where and how your information may have been compromised.</p>
         </div>
         <div >
-            <img src={avatar} alt="" />
+          <img src={avatar} alt="" />
         </div>
       </div>
-      <BreachCards/>
+      <div className='email-data-part'>
+        <div >
+          <img src={avatar1} alt="" />
+        </div>
+        <div className='data-alert-content'>
+          <h1 className='data-content'>0</h1>
+          <h1 className='data-content'>Data Breach Alert</h1>
+          <p>Good news — no breaches found! This email address was not found in any of the data breaches listed in Have I Been Pwned. Nothing to worry about!</p>
+        </div>
+      </div>
+      <BreachCards />
     </div>
   )
 }
